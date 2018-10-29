452bb898c04e019449bb76319cf94c1582c5f1b5
Cara Morelli <caramorelli7@gmail.com>
2018-10-29 16:50:20 -0400

    learn how to use capybara

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 class Application < Sinatra::Base
   # Write your code here!
 
+  get '/' do
+    erb :index
+  end
+
+  get '/page' do
+    erb :greet
+  end
+
+  post '/greet' do
+    erb :greet
+  end
+
 end
